8ecf1cf524e5083d18a33ede62244f9cb178125f
hammadai <hammadshakeel61@gmail.com>
2024-10-16 21:43:24 +0500

    done

--- 4kimamelun.py
+++ 4kimamelun.py
@@ -1,16 +1,15 @@
 b=0
 a=0
-x=[1,7,6,4,7,9,18,3,5,1,7]
-n=0
-for i in range(x[2]):
+x=[1,1,1,1,1,1,1,3000,10,3000]
+for i in list(x):
     n=0
     while i != 0:
         if i<=0:
             break
-        i-=1
-        n+=1
+        i=i-1
+        n=n+1  
     if a<=n:
         a=n
-        if b<=n:
-            b=a
-print(a+b)
+        if b<a and b!=a:
+            b,a=a,b
+print("misterrr beeeassstt",a+b, "ooooooOOooo")
