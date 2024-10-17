792af13e8e5ed5bc01f59ec32f98da6096657774
hammadai <hammadshakeel61@gmail.com>
2024-10-17 17:48:15 +0500

    d

--- safety net/4kimamelun.py
+++ safety net/4kimamelun.py
@@ -1,15 +1,15 @@
 b=0
 a=0
-x=[1,1,1,7,4,0,7,4,3,44,44]
-for i in list(x):
+x=[1,1,1,7,4,0,100,44,3,100,44]#list of varring numbers
+for i in list(x):#looping ever
     n=0
     while i != 0:
         if i<=0:
             break
-        i=i-1
-        n=n+1  
-    if a<=n:
+        i-=1
+        n+=1  
+    if a<n and b!=n:
         a=n
-        if b<a and b!=a:
-            b,a=a,b
-print("misterrr beeeassstt",a+b, "ooooooOOooo")
+    if b<a:    
+        b,a=a,b
+print(a+b)
