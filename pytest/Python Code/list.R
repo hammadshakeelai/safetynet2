1b411e7ccaeba24530740c773ba1e3960a8cd633
hammadai <hammadshakeel61@gmail.com>
2024-10-22 00:32:39 +0500

    v1

--- pytest/Python Code/list.R
+++ pytest/Python Code/list.R
@@ -7,7 +7,7 @@ list=[2,4,6,7,7,6,6,4]#to store number in list----------------------------------
 list.sort() # Output: [1, 2, 4, 9]---------------------------------------------------------SORT NUMBERS ASCENDING
 list.reverse()  # Output: [9, 4, 2, 1]-----------------------------------------------------SORT NUMBERS DESCENDING
 squares=[x**2 for x in range(1, 6)]#formula with varing value- Output: [1, 4, 9,16]--------TRY NUMBERS IN FORMULA
-#for fruit in fruits:#prints every thing in fruits if ""print(fruit)"" is written----------USE LIST METHOD
-#if "apple" in fruits:#--------------------------------------------------------------------LOOKS FOR A STRING IN LIST
-#print(len(fruits))  #  len(list_name) #to find number things inside list------------------COUNTS LIST_LENGTH
-#if i not in my_list:#-------------------------
+for fruit in fruits:#prints every thing in fruits if ""print(fruit)"" is written----------USE LIST METHOD
+if "apple" in fruits:#--------------------------------------------------------------------LOOKS FOR A STRING IN LIST
+print(len(fruits))  #  len(list_name) #to find number things inside list------------------COUNTS LIST_LENGTH
+if i not in my_list:#---------------------------------------------------------------------CHECKS IN LIST THAT IT IS NOT INIT
--- vscode/biggest two distinct valuesadded.py
+++ vscode/biggest two distinct valuesadded.py
@@ -1,11 +1,10 @@
-my_list = [20, 7, 2, 4, 18, 18, 20, 7, 2, 4, 18, 18, 20, 7, 2, 4, 18, 18, 20]
+my_list = [20, 7, 2, 4, 18, 88, 20, 7, 2, 4, 18, 18, 20, 7, 2, 4, 18, 18, 20]
 k=0
-for j in range(2):
+for j in range(2):#repeats two times
     for i in range(1000,-1,-1): # Maximum integer values is 1000
         if i in my_list:
-            j=i
-            print(j)
-            k=k+j
+            print(i)
+            k=k+i
             break
     while i in my_list:
         my_list.remove(i)
