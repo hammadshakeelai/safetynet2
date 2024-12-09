72a9cf2b44bb36e61bcdddd547d9c1301b21e5a5
hammadshakeelai <hammadshakeel61@gmail.com>
2024-12-09 22:37:23 +0500

    readme.md added

---
+++ safetynet23/vscode/G_1_.py
@@ -0,0 +1,11 @@
+print("""      gcnbhmbvj
+      nhgcvhj\\n
+\"""htrghctgvhb\"""
+      'cjtrgfhgnf gb'
+      "xgfvb"
+      vyfhgvbng
+      yyjtjhfngbn 
+      tyjtjhdsxci67uliuolk"""+'''xxxxh fjdvkmc
+      ghbfgvc
+      gbfvdcu
+      jnyhbgvf''')
