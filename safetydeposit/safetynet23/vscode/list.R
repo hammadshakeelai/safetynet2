951ac5125a8acac021cd7cf81f5cfad77fb1fe66
hammadshakeelai <hammadshakeel61@gmail.com>
2024-12-16 23:32:14 +0500

    new

--- safetydeposit/safetynet23/vscode/list.R
+++ safetydeposit/safetynet23/vscode/list.R
@@ -1,20 +1,14 @@
-list_name=["doremon","FUTURE","MIMIKO","sex mLE","5y"]#------------------------------------STORE STRINGS
 list_name.append("anything")#to add to list------------------------------------------------ADD
 list_name.insert(1, "an3ything")#to add on a specific index--------------------------------PUT IN
 list_name.remove("anything")#--------------------------------------------------------------REMOVE
 list_name.pop(2)#remove anything that was on third [position]------------------------------DELETE INDEX
-list=[2,4,6,7,7,6,6,4]#to store number in list---------------------------------------------STORE NUMBERS
 list.sort() # Output: [1, 2, 4, 9]---------------------------------------------------------SORT NUMBERS ASCENDING
 list.reverse()  # Output: [9, 4, 2, 1]-----------------------------------------------------SORT NUMBERS DESCENDING
 squares=[x**2 for x in range(1, 6)]#formula with varing value- Output: [1, 4, 9,16]--------TRY NUMBERS IN FORMULA
-for fruit in fruits:#prints every thing in fruits if ""print(fruit)"" is written----------USE LIST METHOD
 if "apple" in fruits:#--------------------------------------------------------------------LOOKS FOR A STRING IN LIST
-print(len(fruits))  #  len(list_name) #to find number things inside list------------------COUNTS LIST_LENGTH
-if i not in my_list:#---------------------------------------------------------------------CHECKS IN LIST THAT IT IS NOT INIT
 unpaired ^= number
  if A.count(number) == 1:
 print(format(dj,".2f"))
-import random
 s=random.randint(1,999)
 def hello(to="World."):#to hard code hallo to "to" if nothing is given
 name=name.strip()#to remove spaces from string
@@ -22,19 +16,19 @@ name=name.capitalize()#to capitalize first alphabet
 name=name.title()#to capatilize first letter of every word
 first, last =name.split(" ")#splits one variable in two 
 pow(n, .5)#n power 5
-from  cubemap import cubeprintx
+from  file import function
 print(f"{z:,}")#to add , after 3 digits of number
 print(f"{z:.2f}")#goes upto two decimal points 
 c[::-1]#to print it oppsitely
 math .floor .ceil .pi .radians .sin .cos
 
-{and continue finally is raise
-as def for lambda return
-assert del from None True
-async elif global nonlocal try
-await else if not while
-break except import or with
-class False in pass yield}
+{[and continue finally]' is'' raise'
+'as '[def] [for] 'lambda '[return]
+'assert '[del] [from]' None '[True]
+'async '[elif global]' nonlocal '[try]
+'await' [else if not while]
+[break] [except import or] ['with']
+'class' [False] [in] 'pass'' yield'}
 
 import matplotlib.pyplot as plt
 sizes = [25, 30, 35, 10]
@@ -49,3 +43,4 @@ random.randrange(1, 10, 2)
 random.shuffle([12, 34, 56, 60])
 random.choice([1, 2, 3, 4, 5])
 random.sample("abcdefghijklmnopqrstuvwxyz", 15)
+combined_list=[*list1,*list2,*list3]
