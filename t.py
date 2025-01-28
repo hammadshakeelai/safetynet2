d4a196e10360a596303216b17eab958de54d8373
hammadshakeelai <hammadshakeel61@gmail.com>
2025-01-28 19:33:12 +0500

    final probabaly

---
+++ t.py
@@ -0,0 +1,229 @@
+m='''1 .Premium Courses for free
+2. 22 TB BIG COLLECTION PREMIUM COURSE 🎭 mega link
+3.22 TB BIG COLLECTION PREMIUM COURSE 🎭 drive linkAll gfg courses 
+4. Striver live classes DSA problem solving
+5. Placement 100 interview preparation
+6.Data structures with python
+7.Os DBMS cn for sde interview
+8. Self placed dsa
+9.Java foundation
+10.Fundamentals of java
+11.Python foundation
+12.Java app development -winter training
+13. C programming
+14. Amazon sde test series
+15 .  Java backend
+16. Java lecturesGfg completed all courses 
+17.All courses by Angela yu course
+18. iNeuron - Full Stack Web Development with Python in Hindi
+19. Full stack block chain development 
+20. Full stack data development 
+21. Full stack javascript developer
+22. Job ready big data bootcamp
+23. Mastering data science
+24. Full stack web development 2.0 
+25.  Five  thousands+ cheat notes 
+26.Java DSA by hitesh chaudary
+27.Scaler java
+28. Placement materials 1tb drive link 
+29. Farz - Data Structures & Algorithms Fellowship
+30. All 𝗣𝗬𝗧𝗛𝗢𝗡 𝗠𝗘𝗚𝗔 𝗖𝗢𝗨𝗥𝗦𝗘
+31. Huge Road Maps Collections for Developers
+32. [AppliedAI] Machine Learning Online Course 
+33. Full stack web development english 
+34. 100+ Paid Courses Drive Link
+35. Made easy gate mega link 
+35 . unacademy, gateacademy
+36. Ace  gate  
+37.GATEFLIX [CSE]
+38.extra high quality education 100 gb  Drive link
+39. Namaste node js  
+40. Gate ESE Course Download  mega link
+41. devops complete course 
+42.Mera placement hoga course mega link 
+43. 📚 All Type of Campus Placement Material 📚
+44.Programming and Data Structures - Subbarao Lingamgunta - GATE 2024 Mega link
+45.GAME DEVELOPMENT COURSELEARN HOW TO MAKE GAMES IN UNITY!  ✅Mega link
+46. Master the Coding Interview_ Data Structures
+47.[CodingBlocks] Android App Development Master Course 
+48.Coding Blocks Web Development with Python-Django
+49.Coding Blocks - Master Interview Questions for FAANG & Product Companies
+50.[Coding Blocks] Data Structures in Real Life Projects
+51. Data Science Master Course code blocks 
+52. CodeEater - Blockchain Easy Download
+53. Leet code courses 
+54. Machine learning 🫥🫥
+55. Coding ninjas full stack web development 
+56. Coding blocks - Competitive programming
+57.[Coding Ninjas] Advanced Front-End Web Development with React
+58.PrepBytes courses 
+59. 100 Days of Code The Complete Python Pro Bootcamp
+60. [Coding Ninjas] Java Foundation with Data Structures & Algorithms[English & Hindi]
+61. [CodingNinjas] Python Foundation with Data Structures & Algorithms
+62. Complete delta course Apna  college Part 1 Apna college Part 2
+63.📚 Complete Front end resources
+64.React native courses
+65.🔰 22 UDEMY LATEST COURSES 🔰
+66.Code With Mosh Course 170gb course 
+67.COMPLETE ANDROID DEVELOPER COURSE BUILD IN 14 APPS
+68.US wale - kohort
+69.🔰 100GB+ OF EDITING PACK/PRESET PACK | READY TO USE 🔰
+70.GATE - CSE ❤
+71.Spring boot Microservies 24-25 Setup course
+72.Ashok it Gen Ai Latest 2024-25
+73.HiteshChoudharyWebDev
+Complete web development
+PREPINSTA courses
+74. PREPINSTA - ROYAL PASS
+75 . C CPP courses - PREPINSTA
+76.DSA - PREPINSTA
+77. PREPINSTA - TCS NQT Course 2023
+78.PREPINSTA - ELITMUS COURSE
+79.PREPINSTA - Wipro NLTH
+80.Dsa gfg c/c++
+81. Apna college c++
+82.Sigma batch apna college:
+83.Delta batch Apna college 
+84. complete-machine-learning-nlp-bootcamp-mlops
+85. Data Structures and Algorithms with JAVA
+86. Love Babbar Oops unacademy
+87.🔰 PyTorch for Deep Learning Bootcamp
+88.Abdul Bari sir java programming
+89.[IIBM institute] ai and machine learning
+90.ALL CODING BLOCKS COURSES IN ONE LINK
+91.[Coding Ninjas] Competitive Programming
+92.ZeroToMastery - Complete Cybersecurity Bootcamp
+95 .pwgatecse
+96 .CSE GATE PW COMPLETE BATCH
+97 .Gate made easy
+98 .GateWayClassesCourse
+99 .MADE EASY GATE CSE FULL COURSE
+100 .Love babar DSA supreme 1.0
+101 . Abdul Bari : Courses 
+All courses 
+102 .Java for Beginners - Learn all the Basics of Java
+103 .[iNeuron] Full Stack JavaScript Bootcamp 2.0
+104 .SCALAR : DATA SCIENCE INTERMEDIATE
+105 .Algocamp - Master Complete Java + DSA + Backend + System Design
+106 .Artificial Intelligence A-Z 2024: Build 5 AI (incl. ChatGPT)
+107 .Scaler data science & machine learning 
+108 .All python courses
+109 .Namaste React From Zero to Hero
+110 .All types of leetcode courses 
+111 .Data Science/Artificial Intelligence
+112 .Apna* Co11ege Java DSA
+113 .Pwskills Web Development 
+Full Stack Development Course - Hinglish
+114 .ALL TECHNICAL SAGAR COURSES
+115 .Hacking 24.65 GB MEGA Courses
+116 .100 GB+ BIG PACK EDITING ASSETS FREE DOWNLOAD
+117 .A@shok IT AWS
+118 .A@shok IT devops
+119 .Azure Devops 
+120 .AWS fundamentals for Beginners
+121 .Kubernetes Deep drive
+122 .edureak Devops
+123 .AWS real Time
+124 .Complete SQL Mastery
+125 .Machine learning and complete python a to advanced
+126 .Go ml
+127 .pw warehouse
+128 .pw dsa for ai 2025
+129 . machine learning pw
+130 .Pw - IP ADRESSING
+131 .Gate test series
+132 .Note books from gate series
+133 .Notes and PDFs
+Web dev
+134 .Back-End Domination: Create Efficient Back-End.
+135 .Namaste Frontend System Design 🚀 Ilaps
+136 .DJANGO
+137 .JavaScript by Andrei neagoie
+138 .Ineuron FullStack Javascript web development Hitesh Chaudhary
+139 .Ashok IT AWS
+140 .Ineuron Full stack web with python
+141 .Love babbar
+142 .Chai aur code : hitesh chaudary
+143 .Sheriyans coding frontend course
+144 .Mean Stack
+145 .Pw skills zipped version
+146 .Anuj bhaiya
+147 .Tutedude courses Python & data science
+148 .Chai aur code : hitesh chaudary
+149 .Python & data structures
+150 .Pw data science master courses
+151 .Python Abdul bari
+152 .Python code with mosh
+153 .Ineuron data science master course
+154 .Deep learning by IIT Madras professors
+155 .iNeuron - Big Data Bootcamp (Job Guaranteed)
+156 .Applied ai machine learning
+157 .iNeuron - Full Stack Data Analytics
+158 .INeuron.ai - Full Stack Blockchain Development
+159 .iNeuron - DSA for FAANG Preparation with Python and JavaScript
+160 .Pw skills web development
+161 .Data science by IIT Madras professor
+162 .Pw data science master course
+163 .Scaler edge
+164 .Ethical hacking by udemy
+165 .DSA gfg c/c++ Hacking courses
+166 .Ethical hacking complete masterclass one shot
+167 .Social engineering
+168 .Ethical hacking using Linux and termux
+169 .Ethical hacking by cyber mentor[ hamervic]
+170 .Parrot security os
+171 .Android hack by hacker world
+172 .Improve cyber security skills with counter -terrorist financings 
+173 .Carding
+174 .Web hacking
+175 .Hacking courses by Decses
+176 .Computer hacking
+177 .Udemy android app hacking black belt edition 2022
+178 .Certified ethical hacking
+179 .Wifi hacking
+180 .Web security & bug bounty learn penetration testing by udemy
+181 .REACT NATIVE COURSES
+182 .500 tb For complete course 
+183  .Data scientist all courses
+184 .500gb all course
+185  .Naresh it aws full course
+186 .Aws devops course
+187 .Android development using kotlin'''
+n=m.split('\n')
+pp='''Fundamentals of java
+Python foundation
+C programming
+Java DSA by hitesh chaudary
+Farz - Data Structures & Algorithms Fellowship
+[AppliedAI] Machine Learning Online Course
+Programming and Data Structures - Subbarao Lingamgunta - GATE 2024 Mega link
+Leet code courses
+Machine learning 🫥🫥
+Coding blocks - Competitive programming
+100 Days of Code The Complete Python Pro Bootcamp
+[CodingNinjas] Python Foundation with Data Structures & Algorithms
+[IIBM institute] ai and machine learning
+[Coding Ninjas] Competitive Programming
+Abdul Bari : Courses
+Java for Beginners - Learn all the Basics of Java
+Algocamp - Master Complete Java + DSA + Backend + System Design
+Artificial Intelligence A-Z 2024: Build 5 AI (incl
+Scaler data science & machine learning
+All types of leetcode courses
+Data Science/Artificial Intelligence
+Apna* Co11ege Java DSA
+Machine learning and complete python a to advanced
+pw dsa for ai 2025
+machine learning pw
+DJANGO
+Python & data structures
+Deep learning by IIT Madras professors
+Applied ai machine learning'''
+# mylist=['Fundamentals of java', 'Python foundation', 'C programming', 'Java DSA by hitesh chaudary', 'Farz - Data Structures & Algorithms Fellowship', '[AppliedAI] Machine Learning Online Course', 'Programming and Data Structures - Subbarao Lingamgunta - GATE 2024 Mega link', 'Leet code courses', 'Machine learning 🫥🫥', 'Coding blocks - Competitive programming', '100 Days of Code The Complete Python Pro Bootcamp', '[CodingNinjas] Python Foundation with Data Structures & Algorithms', '[IIBM institute] ai and machine learning', '[Coding Ninjas] Competitive Programming', 'Abdul Bari : Courses', 'Java for Beginners - Learn all the Basics of Java', 'Algocamp - Master Complete Java + DSA + Backend + System Design', 'Artificial Intelligence A-Z 2024: Build 5 AI (incl', 'Scaler data science & machine learning', 'All types of leetcode courses', 'Data Science/Artificial Intelligence', 'Apna* Co11ege Java DSA', 'Machine learning and complete python a to advanced', 'pw dsa for ai 2025', 'machine learning pw', 'DJANGO', 'Python & data structures', 'Deep learning by IIT Madras professors', 'Applied ai machine learning']
+b=[]
+for i in n:
+    print(i)
+    mm=input("Enter ] OR press enter: ")
+    if mm==']':
+        b.append(i)
+print(b)
