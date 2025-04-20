59a84dc13e24338ae397702a2ee8a482c4116441
hammadshakeelai <hammadshakeel61@gmail.com>
2025-04-20 23:37:59 +0500

    y

--- my_codes/dynamic programming/total size.py
+++ my_codes/dynamic programming/total size.py
@@ -1,4 +1,4 @@
-str1='houseesuoh'    
-for i in range(2,len(str1)+1):
+str1='hyhouseesuohyh'    
+for i in range(1,len(str1)+1):
     if str1[:i]==str1[:i][::-1]:
         print(f'{str1[:i]} is a palindrome')
